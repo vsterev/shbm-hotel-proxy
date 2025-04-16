d0c89b08024f8ff348016714571d86eeb7f3f8b5
Vassil Shterev <vsterev@gmail.com>
2025-04-17 01:46:27 +0300

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,8 +19,7 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:vsterev/shbm-hotel-proxy.git",
       path: "/home/vsterev/git/shbm/pm2/shbm-hotel-proxy",
-      "post-deploy":
-        "yarn && yarn build && pm2 startOrReload ecosystem.config.cjs --env production",
+      "post-deploy": "yarn && yarn build",
     },
   },
 };
