eeb5646b46e1fc91bc06bf300207f9564eeb576c
Vassil Shterev <vsterev@gmail.com>
2025-04-17 02:26:33 +0300

    last 4

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
       repo: "git@github.com:vsterev/shbm-hotel-proxy.git",
       path: "/home/vsterev/git/shbm/pm2/shbm-hotel-proxy",
       "post-deploy":
-        "source ~/.nvm/nvm.sh && nvm use 20 && YARN=/home/vsterev/.nvm/versions/node/v22.14.0/bin/yarn && $YARN && $YARN build && pm2 startOrReload ecosystem.config.cjs --only shbm_hotel_proxy",
+        "source ~/.nvm/nvm.sh && nvm use 22 && YARN=/home/vsterev/.nvm/versions/node/v22.14.0/bin/yarn && $YARN && $YARN build && pm2 startOrReload ecosystem.config.cjs --only shbm_hotel_proxy",
     },
   },
 };
