e287583a39204ba970537d236f681c6c475feda6
Vassil Shterev <vsterev@gmail.com>
2025-04-22 20:22:57 +0300

    change ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,21 @@
 module.exports = {
 	apps: [
 		{
-			name: "shbm-hotel-proxy",
-			script: "./dist/src/app.js",
+			name: 'shbm-hotel-proxy',
+			script: './dist/src/app.js',
 			instances: 1,
 			wait_ready: true,
-			exec_mode: "cluster",
+			exec_mode: 'cluster',
 		},
 	],
 	deploy: {
 		production: {
-			user: "vsterev",
-			host: "192.168.10.10",
-			ref: "origin/master",
-			repo: "git@github.com:vsterev/shbm-hotel-proxy.git",
-			path: "/home/vsterev/git/shbm/pm2/shbm-hotel-proxy",
-			"post-deploy":
-				"source ~/.nvm/nvm.sh && nvm use 22 && YARN=/home/vsterev/.nvm/versions/node/v22.14.0/bin/yarn && $YARN && $YARN build && pm2 startOrReload ecosystem.config.cjs --only shbm-hotel-proxy",
+			user: 'vsterev',
+			host: '192.168.10.10',
+			ref: 'origin/master',
+			repo: 'git@github.com:vsterev/shbm-hotel-proxy.git',
+			path: '/home/vsterev/git/shbm/pm2/shbm-hotel-proxy',
+			'post-deploy': 'yarn && yarn build && pm2 startOrReload ecosystem.config.cjs --only shbm-hotel-proxy',
 		},
 	},
 };
